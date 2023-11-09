f2eca27d5822af0a70e1407b7215e8fbbd4df223
ahmed-hamed456 <120836776+ahmed-hamed456@users.noreply.github.com>
2023-11-10 01:17:17 +0200

    Add project files.

---
+++ RepositoryPatternWithUOW.Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,31 @@
+﻿using RepositoryPatternWithUOW.Core.Consts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternWithUOW.Core.Interfaces
+{
+    public interface IBaseRepository<T> where T : class 
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        T Find(Expression<Func<T, bool>> Match, string[] includes = null);
+        IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, string[] includes = null);
+        IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, int take,int skip);
+        IEnumerable<T> FindAll(Expression<Func<T, bool>> criteria, int? take,int? skip,
+            Expression<Func<T,object>> orderBy = null,string orderByDirection = OrderBy.Ascending);
+
+        T Add(T entity);
+        IEnumerable<T> AddRange(IEnumerable<T> entities);
+
+        T Update(T entity);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+        void Attach(T entity);
+        int count();
+        int count(Expression<Func<T, bool>> criteria);
+    }
+}
